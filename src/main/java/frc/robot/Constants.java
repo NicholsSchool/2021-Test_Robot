5cc10758c4276ed195eceb0cc01c5bd20c1bcaf2
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2021-01-23 11:41:00 -0500

    Added some IDs in Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,14 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final int PDP_ID = 50
+    public static final int TALON_ID = 22
+    public static final int VICTOR_ID = 21
+
+    public static final int POT_ANALOG_IN_ID = 0;
+    public static final int SPARK_PWM_ID = 0;
+    public static final int LIMIT_SWITCH_DIO_ID = 0;
+
+}
