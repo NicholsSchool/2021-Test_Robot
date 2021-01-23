157b7bc29e25e15020e7aff5145eadf5be9dab43
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2021-01-23 12:48:20 -0500

    Oops. RobotMap, not Constants

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,3 +1,11 @@
 public class RobotMap {
     
+    public static final int PDP_ID = 50
+    public static final int TALON_ID = 22
+    public static final int VICTOR_ID = 21
+
+    public static final int POT_ANALOG_IN_ID = 0;
+    public static final int SPARK_PWM_ID = 0;
+    public static final int LS_DIO_ID = 0;
+    
 }
